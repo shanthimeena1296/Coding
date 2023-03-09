1f6495d5070f9e2178ea71ed226b2407c8544d41
Shanthi Meena Arumugam <smarumug@uwaterloo.ca>
2023-03-08 23:39:06 -0500

    Adding ASCII code

--- ASCII.py
+++ ASCII.py
@@ -1,8 +1,11 @@
 def draw_stairs(n):
     # do something
-    for i in range(0,n):
-        for j in range(i):
-            print(i,j)
-            print("I")
-            j=j+1
-#                 print(" ")
+    for i in range(n):
+        print(" " * i, end=" ")
+        print("I")
+#         for j in range(i):
+#             print(i,j)
+#             # print("I")
+#             j=j+1
+# #                 print(" ") 
+draw_stairs(3)
